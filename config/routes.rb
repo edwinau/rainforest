cb1dcd60c3e4b22d6aa7484d5cac05806f21efc7
EDWIN AU <edwinau@rogers.com>
2017-04-19 23:44:19 -0400

    Writing review routes, controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
 
 
+  get 'reviews/show'
+
+  get 'reviews/create'
+
+  get 'reviews/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :products
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
-
+  resources :products do
+    resources :reviews, only: [:show, :create, :destroy]
+  end
 
 end
